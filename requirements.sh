2140c8b917904c9cf11848b1be5124dff0e54493
Michael Chow <machow@princeton.edu>
2017-03-03 21:09:01 -0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,7 @@
 apt-get update
 apt-get install -y clang
 
-update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
-update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
+mkdir -p $HOME/.R && printf "CXX=clang++\nCC=clang\n" > $HOME/.R/Makevars
+
+#update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
+#update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
