db6b053f2f2f2ae32016b41ef43404e8187d87ab
Michael Chow <machow@princeton.edu>
2017-03-03 23:11:19 -0500

    Update requirements.r

--- requirements.r
+++ requirements.r
@@ -1,6 +1,6 @@
 library(devtools)
 
-install.packages(c("Rcpp", "RcppEigen", "rstan", "rstanarm"))
+install.packages(c("Rcpp", "RcppEigen", "rstan"))
 # install the latest loo package that fixed a bug (Jan 18, 2017)
 install_github("stan-dev/loo", build_vignettes = TRUE)
 install_github("CCS-Lab/hBayesDM")
