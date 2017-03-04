8e2360adfa03a67cf752f750a1b22586e966ae57
Michael Chow <machow@princeton.edu>
2017-03-03 21:57:28 -0500

    Update requirements.r

--- requirements.r
+++ requirements.r
@@ -1,5 +1,5 @@
 library(devtools)
 
 # install the latest loo package that fixed a bug (Jan 18, 2017)
-#install_github("stan-dev/loo", build_vignettes = TRUE)
-#install_github("CCS-Lab/hBayesDM")
+install_github("stan-dev/loo", build_vignettes = TRUE)
+install_github("CCS-Lab/hBayesDM")
