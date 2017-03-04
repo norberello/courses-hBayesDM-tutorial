197c873e8108633470b0bac7a21f579910329ca3
Michael Chow <machow@princeton.edu>
2017-03-04 10:25:00 -0500

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
 apt-get update
-apt-get install -y clang
+apt-get install -y --no-install-recommends clang
 
-mkdir -p $HOME/.R && printf "CXX=clang++\nCC=clang\n" > $HOME/.R/Makevars
+mkdir -p $HOME/.R && printf "CXX=clang++ -ftemplate-depth-256\nCC=clang\n" > $HOME/.R/Makevars
